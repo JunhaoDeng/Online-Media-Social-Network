54620e00221cb890680fe7c8493124b033329a26
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2023-12-17 16:33:14 -0800

    Initial commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.128.0.2:9200"
+    ES_USERNAME = "junhaode"
+    ES_PASSWORD = "wj781005"
+	GCS_BUCKET = "around_2"
+)
